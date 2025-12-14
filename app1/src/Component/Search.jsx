c41b7c10b6137fae1724e6a354602fac9404866b
pransusojitra <pransusojitra1@gmail.com>
2025-12-14 20:41:02 +0530

    Add new page

--- app1/src/Component/Search.jsx
+++ app1/src/Component/Search.jsx
@@ -10,8 +10,8 @@ const Search = () => {
                     <p>Get in touch with our experts toda</p>
 
                     <div class="calbtn2 d-flex justify-content-center align-items-center flex-lg-row flex-md-row flex-column gap-2">
-                        <button type="button" className='calbtn w-50 h-25 border rounded-1'><i class="fa-solid fa-phone pe-4 py-2"></i>Call Now</button>
-                        <button type="button" className='calbtn w-50 h-25 border rounded-1'><i class="fa-regular fa-envelope pe-4 py-2"></i>Get Quote</button>
+                        <button type="button" className='calbtn w-50 h-25 border rounded-1' style={{color: "#0b1c40"}}><i class="fa-solid fa-phone pe-4 py-2"></i>Call Now</button>
+                        <button type="button" className='calbtn w-50 h-25 border rounded-1' style={{color: "#0b1c40"}}><i class="fa-regular fa-envelope pe-4 py-2"></i>Get Quote</button>
                     </div>
                 </div>
             </div>
